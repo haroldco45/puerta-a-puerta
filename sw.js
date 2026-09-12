48e2433261fe0aed3e162f47038d720cbcdf8dc9
haroldco45 <68300211+haroldco45@users.noreply.github.com>
2026-09-12 16:45:57 -0500

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'puerta-a-puerta-v1';
+const ARCHIVOS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ARCHIVOS).catch(() => {})).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// Cache primero para el shell; red con respaldo de caché para lo demás (fuentes).
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit => {
+      if (hit) return hit;
+      return fetch(e.request).then(res => {
+        const copia = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copia)).catch(() => {});
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
